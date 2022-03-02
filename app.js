c7c85920c34fe2bd8d3f434a104976b9f4ecf451
VictoriaQuintero <Victoria170460@gmail.com>
2022-03-02 11:42:37 -0400

    responsive completo :)

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
 $(document).ready(function () {
+  //-----------------sidebarCLICK-----------------
+  if($(window).width()<760){
+    $('.nav-side').css({ transform: 'translateX(0)' }).toggle(500);
+
+    $('header nav #sideButton').click(function () {
+      $('.nav-side').toggle(500);
+    });
+  }else{
+    $('.nav-side').show();
+  }
+
   // ----------SLIDER----------------------
   let div = $('.div-sliders');
   let divs = div.find('div[id=sliders]').length;
-  let auto=0;
-  div.css('width', divs * 100 +'%');
+  let auto = 0;
+  div.css('width', divs * 100 + '%');
 
   $('div[id=sliders]:last').insertBefore('div[id=sliders]:first');
-  div.css({left: '-100%'});
+  div.css({ left: '-100%' });
 
-  $('#botonRight').click(function () { 
+  $('#botonRight').click(function () {
     div.animate({
-      left:'-200%'
-    },900, function(){
+      left: '-200%'
+    }, 900, function () {
       $('div[id=sliders]:first').insertAfter('div[id=sliders]:last');
-      div.css({left: '-100%'});
+      div.css({ left: '-100%' });
     })
-    auto=0;
+    auto = 0;
   });
-  $('#botonLeft').click(function () { 
+  $('#botonLeft').click(function () {
     div.animate({
-      left:0
-    },900, function(){
+      left: 0
+    }, 900, function () {
       $('div[id=sliders]:last').insertBefore('div[id=sliders]:first');
-      div.css({left: '-100%'});
+      div.css({ left: '-100%' });
     })
-    auto=0;
+    auto = 0;
   });
   setInterval(() => {
     auto++;
@@ -37,30 +48,52 @@ $(document).ready(function () {
       })
     }
   }, 3000);
+  
   // ----------------------CARRUSEL------------------------
   let contenedor = $('.contenedor-servicios');
   let servicios = contenedor.find('div[class=servicios]').length;
-  let cont=0;
-  console.log(contenedor.css({width: servicios * 20 + '%'}));
-  contenedor.css({left:'-20%'});
+  let cont = 0;
+  let left;
+  if($(window).width()<1024){
+    contenedor.css({ width: servicios * 30 + '%' });
+    contenedor.css({ left: '-30%' });
+  }else{
+    contenedor.css({ width: servicios * 20 + '%' });
+    contenedor.css({ left: '-20%' });
+  }
 
-  $('#buttonLeft').click(function () { 
+  $('#buttonLeft').click(function () {
     contenedor.animate({
-      left:0
-    },900,function(){
+      left: 0
+    }, 900, function () {
       $('div[class=servicios]:last').insertBefore('div[class=servicios]:first');
-      contenedor.css({left:'-20%'});
+      if($(window).width()<1024){
+        contenedor.css({ left: '-30%' });  
+      }else{
+        
+        contenedor.css({ left: '-20%' });
+      }
     })
-    cont=0;
+    cont = 0;
   });
-  $('#buttonRight').click(function () { 
+  $('#buttonRight').click(function () {
+    if($(window).width()<1024){
+      left='-60%';  
+    }else{
+      left='-40%';
+    }
     contenedor.animate({
-      left:'-40%'
-    },900,function(){
+      left: left
+    }, 900, function () {
       $('div[class=servicios]:first').insertAfter('div[class=servicios]:last');
-      contenedor.css({left:'-20%'});
-    }) 
-    cont=0;
+      if($(window).width()<1024){
+        contenedor.css({ left: '-30%' });  
+      }else{
+        
+        contenedor.css({ left: '-20%' });
+      }
+    })
+    cont = 0;
   });
 
   setInterval(() => {
@@ -74,27 +107,26 @@ $(document).ready(function () {
       }) 
     }
   }, 3000);
-  // ----------------------------------------------------
+
+  // ----------------------CAMBIO DE NAV------------------------------
   $(window).scroll(function () {
     if ($(window).scrollTop() > $('nav').height()) {
       $("nav").addClass("sticky");
     } else {
       $("nav").removeClass("sticky");
     };
-
-    // ------------------------------------------------
-
-    if ($(window).scrollTop() > $('.section2').height()+300) {
-
+    
+    // ---------------------CONTADOR ANIMADO---------------------------
+    if($(window).scrollTop() > $(".section2").offset().top-$('.section2').height()){
       $('.Count').each(function () {
         let $this = $(this),
           countTo = $this.attr('data-value');
-
+  
         $({ countNum: $this.text() }).animate({
           countNum: countTo
         },
           {
-            duration: 2000,
+            duration: 1000,
             easing: 'linear',
             step: function () {
               $this.text(Math.floor(this.countNum));
@@ -102,28 +134,27 @@ $(document).ready(function () {
             complete: function () {
               $this.text(this.countNum);
             }
-
+  
           });
-
+  
       });
     }
-
-    // ---------------------------------------------
-    if ($(window).scrollTop() > $('.section2').height() + 400) {
+    
+    // ---------------------ANIMACION DE LOS PLANES------------------------
+    if($(window).scrollTop() > $(".section2").offset().top - $('.section2').height()){
       $('.section2 .planes img').addClass('cambio');
     }else{
       $('.section2 .planes img').removeClass('cambio');
     }
-
-    // --------------------------------------------------------------
-    if($(window).scrollTop() > 1300){
+    // -------------------ANIMACION IMAGEN SECCION 3-----------------------------
+    if ($(window).scrollTop() > $(".seccion3").offset().top - $('.seccion3').height()) {
       $('.seccion3 .containerImg img').addClass('move');
-    }else{
+    } else {
       $('.seccion3 .containerImg img').removeClass('move');
     }
-
   });
-  // -----------------------------------------
+
+  // -----------------Lista desplegable seccion3 ------------------------
   $('.seccion3 .menuDesple ul li p').on("click", function (e) {
     let ul = $(this).next('ul');
     $(this).closest('ul').find('ul').each((index, item) => {
@@ -134,7 +165,7 @@ $(document).ready(function () {
     ul.toggle(500);
   });
 
-  // -------------------------------------------------------
+  // ---------------------Animar boton para hacer scroll en header----------------------------------
 
   $('.buttonSubeBaja button').click(function () {
     $("html, body").animate({ scrollTop: 500 }, 600);
